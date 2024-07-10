bbfc513c81898e0e5c4c49acd12a3244abcced71
IReallyLikeYourPants <michelekryston@gmail.com>
2024-07-10 15:30:56 +0200

    Fix

--- sh_files/reset_db.sh
+++ sh_files/reset_db.sh
@@ -6,14 +6,17 @@ cd ../databases
 # Delete and recreate the attribute_certifier database
 cd attribute_certifier
 rm -f attribute_certifier.db
+sqlite3 attribute_certifier.db < ../commands.sql
 
 # Delete and recreate the data_owner database
 cd ../data_owner
 rm -f data_owner.db
+sqlite3 data_owner.db < ../commands.sql
 
 # Delete and recreate the reader database
 cd ../reader
 rm -f reader.db
+sqlite3 reader.db < ../commands.sql
 
 cd ..
 filename="../src/.env"
@@ -23,7 +26,20 @@ divided_count=$((count / 3))
 find . -type d -name 'authority*' ! -name 'authority1' -exec rm -rf {} +
 rm -f "authority1/authority1.db"
 
-cd ../sh_files
+for i in $(seq 2 $divided_count); do
+    src="authority1"
+    dest="authority$i"
+    cp -r "$src" "$dest"
+done
 
-./create_db.sh
+for i in $(seq 1 $divided_count); do
+    dest="authority$i"
+    cd "$dest"
+    sqlite3 "$dest.db" < ../commands.sql
+    cd ..
+done
+
+echo "✅ Database done!"
+
+
 
